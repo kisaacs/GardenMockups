aeb785097a4bb39f17c3acc639afb65264033e21
Thu <thutra@email.arizona.edu>
2021-12-03 19:07:38 -0700

    Update map print

--- prototype2/js/viewmodel.js
+++ prototype2/js/viewmodel.js
@@ -1,3 +1,4 @@
+
 class ViewModel {
     constructor() {
         this.model = new Model();
@@ -26,37 +27,32 @@ class ViewModel {
       * @param {*} mapId The id of the div that the map will attach to
       */
     createMap(mapId) {
+
         let mymap = L.map(mapId).setView([34.0489, -112.0937], 7);
-		this.selectedData[mapId] = "";
+
+        this.selectedData[mapId] = "";
         L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
             attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
             maxZoom: 18,
             id: 'mapbox/streets-v11',
             tileSize: 512,
             zoomOffset: -1,
-            accessToken: 'pk.eyJ1IjoiYmxhcmEiLCJhIjoiY2tnNzFrNmo2MDMweDJ5cW0zaXJwbWQ1ZyJ9.WydwzOibe0497pQbasuF-A'
-        }).addTo(mymap);
+            accessToken: 'pk.eyJ1IjoiYmxhcmEiLCJhIjoiY2tnNzFrNmo2MDMweDJ5cW0zaXJwbWQ1ZyJ9.WydwzOibe0497pQbasuF-A',
+        }
+            
+        ).addTo(mymap);
         L.control.scale().addTo(mymap);
-    
-        L.simpleMapScreenshoter().addTo(mymap)
 
-        var poly = null;
+        // locate address
         var geocoder = L.Control.geocoder({
             defaultMarkGeocode: true
         }).addTo(mymap);
-            //.on('markgeocode', function (e) {
-            //if (poly) {
-            //    poly.remove();
-            //}
-            //var bbox = e.geocode.bbox;
-            //poly = L.polygon([
-            //    bbox.getSouthEast(),
-            //    bbox.getNorthEast(),
-            //    bbox.getNorthWest(),
-            //    bbox.getSouthWest()
-            //]).addTo(mymap);
-            //mymap.fitBounds(poly.getBounds());
-          
+
+        var bigimage = L.control.BigImage({
+            printControlTitle: 'Download map as image',
+            mapName: mapId
+        }).addTo(mymap);
+
 
         return mymap;
     }
